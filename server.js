be3a8e906afe5b8f5e6e376aaa23c7b5748f118e
vishshukla <vwshukla@gmail.com>
2018-08-26 21:24:50 -0700

    added projects page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    content: 'Portfolio page'
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({
     error: "Error msg"
